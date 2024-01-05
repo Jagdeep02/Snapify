92d891a4b0a1388793ca0dcfdd245d0e3baefab0
Jagdeep Singh <63603286+Jagdeep02@users.noreply.github.com>
2024-01-05 19:31:40 +0530

    Update settings.py

--- mp/settings.py
+++ mp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-llw4cjpd8p_kk4jr!zffwt1grpe!+hzae@&4ehp91jn0ss=*b3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','snapify-duxt-main-n6ztehkxya-df.a.run.app']
 
 
 # Application definition
