e543d376088d32551bcdcca777a3975c2e9dbcce
Jagdeep Singh <63603286+Jagdeep02@users.noreply.github.com>
2024-01-05 19:24:17 +0530

    Update settings.py

--- mp/settings.py
+++ mp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-llw4cjpd8p_kk4jr!zffwt1grpe!+hzae@&4ehp91jn0ss=*b3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -125,8 +125,3 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR)
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
